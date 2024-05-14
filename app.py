6e1970ed100399ae79c2d1a72fbd371f90cce16e
Macarthur Diby <115054586+Camaelo26@users.noreply.github.com>
2024-05-14 12:42:37 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ from flask import Flask, request, render_template, redirect, url_for, flash, ses
 from mysql.connector import Error
 from register import register_user,verify_password
 from connection import create_connection
+from update import update_doctor_inf,update_appointment_status,update_patient_info
+from appointment import get_pending_appointments,cancel_appointment_by_patient,change_appointment_time,request_appointment,request_new_appointment_time,view_my_appointments,get_scheduled_appointments,get_all_appointments, review_pending_appointments_content
+from department import get_doctors_by_department
 import logging
 
 app = Flask(__name__)
@@ -13,7 +16,7 @@ logging.basicConfig(level=logging.DEBUG)
 def home():
     return render_template('index.html')
 
-# Adjust the signup route to pass form directly
+#####
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
     if request.method == 'POST':
@@ -34,7 +37,7 @@ def signup():
         return render_template('sign_up.html')
 
 
-
+#####
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -49,43 +52,42 @@ def login():
         connection = create_connection()
         try:
             cursor = connection.cursor(buffered=True)
-            cursor.execute("SELECT UserID, PasswordHash, Role FROM Users WHERE Username = %s", (username,))
+            cursor.execute("SELECT UserID, PasswordHash, Role FROM users WHERE Username = %s", (username,))
             result = cursor.fetchone()
             if result:
                 user_id, stored_password, role = result
-                print(f"Role: {role}, User ID: {user_id}")  # Debugging print statement
-                try:
-                    if verify_password(stored_password, password):
-                        session['user_id'] = user_id
-                        session['role'] = role
-                        if role == 'Doctor':
-                            print("Redirecting to doctor's menu")
-                            print("Session Role:", session.get('role'))
+                if verify_password(stored_password, password):
+                    session['user_id'] = user_id
+                    session['role'] = role
+                    if role == 'Doctor':
+                        cursor.execute("SELECT DoctorID FROM doctors WHERE UserID = %s", (user_id,))
+                        doctor_result = cursor.fetchone()
+                        if doctor_result:
+                            session['doctor_id'] = doctor_result[0]
                             return redirect('/doctor/menu', code=302)
-                        elif role == 'Patient':
-                            print("Redirecting to patient's menu")
+                        else:
+                            flash('Doctor information not found.', 'error')
+                    elif role == 'Patient':
+                        cursor.execute("SELECT PatientID FROM patients WHERE UserID = %s", (user_id,))
+                        patient_result = cursor.fetchone()
+                        if patient_result:
+                            session['patient_id'] = patient_result[0]
                             return redirect(url_for('patient_menu'))
                         else:
-                            print("Redirecting to generic menu")
-                            return redirect(url_for('generic_menu'))
+                            flash('Patient information not found.', 'error')
                     else:
-                        flash('Invalid username or password', 'error')
-                        print("invalid password or password")
-                except Exception as e:
-                    flash(str(e), 'error')
+                        flash('Unknown user role.', 'error')
+                else:
+                    flash('Invalid username or password', 'error')
             else:
                 flash('Invalid username or password', 'error')
-                print("invalid password or password")
         except Exception as e:
             flash(str(e), 'error')
         finally:
             if connection.is_connected():
                 connection.close()
-            
-    else:
-        return render_template('login.html')
-
-
+    return render_template('login.html')
+#####
 
 
 @app.route('/doctor/menu')
@@ -96,51 +98,112 @@ def doctor_menu():
         flash('Unauthorized access.', 'error')
         return redirect(url_for('login'))
 
-
+##########PATIENT##########
 
 @app.route('/patient/menu')
 def patient_menu():
     if 'role' in session and session['role'] == 'Patient':
-        return 'Welcome to the Patient menu!'
-    flash('Unauthorized access.', 'error')
-    return redirect(url_for('login'))
+        return render_template('patient_menu.html')
+    else:
+        flash('Unauthorized access.', 'error')
+        return redirect(url_for('login'))
+    
+
+
+
+@app.route('/view_appointments')
+def view_appointments_route():
+    patient_id = session.get('patient_id')
+    if not patient_id:
+        flash('No patient ID found in session.', 'error')
+        return redirect(url_for('login'))
+
+    appointments = view_my_appointments(create_connection(), patient_id)
+    #print(f"Appointments passed to template for patient ID {patient_id}: {appointments}")
+
+    return render_template('view_appointments.html', appointments=appointments)
+
+
+@app.route('/update_info', methods=['GET', 'POST'])
+def update_info_route():
+    if request.method == 'POST':
+        first_name = request.form['first_name']
+        last_name = request.form['last_name']
+        dob = request.form['dob']
+        gender = request.form['gender']
+        contact_number = request.form['contact_number']
+        address = request.form['address']
+        email = request.form['email']
+        update_patient_info(create_connection(), session['patient_id'], first_name, last_name, dob, gender, contact_number, address, email)
+        return redirect(url_for('patient_menu'))
+    return render_template('update_info.html')
+
+@app.route('/change_appointment', methods=['GET', 'POST'])
+def change_appointment_route():
+    connection = create_connection()
+    if request.method == 'POST':
+        appointment_id = request.form['appointment_id']
+        new_date = request.form['new_date']
+        change_appointment_time(connection, session['patient_id'], appointment_id, new_date)
+        return redirect(url_for('change_appointment_route'))
+    
+    pending_appointments = get_pending_appointments(connection, session['patient_id'])
+    connection.close()
+    return render_template('change_appointment.html', pending_appointments=pending_appointments)
+
+
+
+@app.route('/request_appointment', methods=['GET', 'POST'])
+def request_appointment_route():
+    if request.method == 'POST':
+        department_name = request.form.get('department_name')
+        doctor_id = request.form.get('doctor_id')
+        desired_date = request.form.get('desired_date')
+
+        if department_name and not doctor_id:
+            doctors = get_doctors_by_department(department_name)
+            return render_template('request_appointment.html', department_name=department_name, doctors=doctors)
+
+        if doctor_id and desired_date:
+            patient_id = session.get('patient_id')  # Assuming the patient_id is stored in the session
+            request_appointment(patient_id, doctor_id, desired_date)
+            message = 'Appointment requested successfully!'
+            return render_template('request_appointment.html', message=message)
+
+    return render_template('request_appointment.html')
+
+
+
+@app.route('/cancel_appointment', methods=['GET', 'POST'])
+def cancel_appointment_route():
+    connection = create_connection()
+    if request.method == 'POST':
+        appointment_id = request.form['appointment_id']
+        cancel_appointment_by_patient(connection, session['patient_id'], appointment_id)
+        return redirect(url_for('cancel_appointment_route'))
+    
+    appointments = get_all_appointments(connection, session['patient_id'])
+    connection.close()
+    return render_template('cancel_appointment.html', appointments=appointments)
+
+@app.route('/request_new_time_route', methods=['GET', 'POST'])
+def request_new_time_route():
+    connection = create_connection()
+    if request.method == 'POST':
+        appointment_id = request.form['appointment_id']
+        new_date = request.form['new_date']
+        request_new_appointment_time(connection, session['patient_id'], appointment_id, new_date)
+        return redirect(url_for('request_new_time_route'))
+    
+    scheduled_appointments = get_scheduled_appointments(connection, session['patient_id'])
+    connection.close()
+    return render_template('request_new_time.html', scheduled_appointments=scheduled_appointments)
+
+########PATIENT MENU END##########
 
 @app.route('/generic/menu')
 def generic_menu():
     return 'Welcome to the Generic menu!'
-
-def review_pending_appointments_content(connection, doctor_id):
-    cursor = connection.cursor()
-    try:
-        query = """
-        SELECT AppointmentID, Patients.FirstName, Patients.LastName, AppointmentDate, Status
-        FROM Appointments
-        JOIN Patients ON Appointments.PatientID = Patients.PatientID
-        WHERE DoctorID = %s
-        ORDER BY AppointmentDate
-        """
-        cursor.execute(query, (doctor_id,))
-        results = cursor.fetchall()
-
-        if results:
-            appointments = []
-            for appt in results:
-                appointment = {
-                    "ID": appt[0],
-                    "Patient": f"{appt[1]} {appt[2]}",
-                    "Date": appt[3].strftime('%Y-%m-%d %H:%M:%S'),
-                    "Status": appt[4]
-                }
-                appointments.append(appointment)
-            return render_template('review_pending_appointments.html', appointments=appointments)
-        else:
-            message = "No pending appointments found for this doctor."
-            return render_template('review_pending_appointments.html', message=message)
-    except Error as e:
-        flash(f"Error fetching appointments: {e}", 'error')
-        return redirect(url_for('doctor_menu'))
-    finally:
-        cursor.close()
 
 
 @app.route('/doctor/review_pending_appointments', methods=['GET', 'POST'])
@@ -182,19 +245,24 @@ def update_doctor_info():
         contact_number = request.form.get('contact_number')
         email = request.form.get('email')
 
-        # Check if any information is provided for update
-        if not any((first_name, last_name, contact_number, email)):
-            flash('No changes submitted.', 'info')
-            return redirect(url_for('update_doctor_info'))
+        print("First Name:", first_name)
+        print("Last Name:", last_name)
+        print("Contact Number:", contact_number)
 
-        # Update the doctor information
-        if update_doctor_inf(connection, doctor_id, first_name, last_name, contact_number, email):
-            flash('Doctor information updated successfully.', 'success')
+
+        if first_name != '' or last_name != '' or contact_number != '':
+            print("Updating doctor information...")
+            # Update the doctor information
+            if update_doctor_inf(connection, doctor_id, first_name, last_name, contact_number,email):
+                flash('Doctor information updated successfully.', 'success')
+            else:
+                flash('Failed to update doctor information.', 'error')
         else:
-            flash('Failed to update doctor information.', 'error')
+            flash('No changes submitted.', 'info')
         return redirect(url_for('update_doctor_info'))
 
     return render_template('update_doctor_info.html')
+
 
 @app.route('/doctor/view_scheduled')
 def view_scheduled_patient_info():
